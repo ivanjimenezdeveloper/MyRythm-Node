639d9ddb4e1097daa8347258f804d410e291965d
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-17 13:37:13 +0200

    getUsuario

--- server/init.js
+++ server/init.js
@@ -13,6 +13,7 @@ const {
   listarListasReproduccionPorUsuario,
 } = require("../db/controller/listaReproduccion");
 const { listarLocalizaciones } = require("../db/controller/localizacion");
+const { getUsuario } = require("../db/controller/user");
 
 const app = express();
 
@@ -61,6 +62,13 @@ const iniciaServidor = () => {
   app.get("/listasReproduccion/:idUsuario", async (req, res, next) => {
     const { idUsuario } = req.params;
     const lista = await listarListasReproduccionPorUsuario(idUsuario);
+
+    res.json(lista);
+  });
+
+  app.get("/usuario/:idUsuario", async (req, res, next) => {
+    const { idUsuario } = req.params;
+    const lista = await getUsuario(idUsuario);
 
     res.json(lista);
   });
