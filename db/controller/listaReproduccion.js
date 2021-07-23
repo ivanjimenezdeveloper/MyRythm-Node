06b9a2aeaca62d1f3f654e93782dd326e684c525
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-23 09:44:13 +0200

    extraerListaMeGusta

--- db/controller/listaReproduccion.js
+++ db/controller/listaReproduccion.js
@@ -1,3 +1,7 @@
+const { crearError } = require("../../utilities/errores");
+const Artista = require("../model/Artista");
+const Cancion = require("../model/Cancion");
+const Genero = require("../model/Genero");
 const ListaReproduccion = require("../model/ListaReproduccion");
 
 const listarListasReproduccionPorUsuario = async (idUser) => {
@@ -25,7 +29,32 @@ const crearListaLikeParaUsuario = async (idUser, sesion) => {
   return true;
 };
 
+const getListaMegusta = async (idUser) => {
+  try {
+    const lista = await ListaReproduccion.findOne({
+      user: idUser,
+      tipo: "Like",
+    }).populate("canciones");
+
+    let listaFormateada = await Cancion.populate(lista, {
+      path: "canciones.genero",
+      model: Genero,
+    });
+
+    listaFormateada = await Artista.populate(listaFormateada, {
+      path: "canciones.artista",
+      model: Artista,
+    });
+
+    const { canciones } = listaFormateada;
+    return canciones;
+  } catch (err) {
+    throw crearError("No se ha podido extraer la lista de me gusta", 500);
+  }
+};
+
 module.exports = {
   listarListasReproduccionPorUsuario,
   crearListaLikeParaUsuario,
+  getListaMegusta,
 };
